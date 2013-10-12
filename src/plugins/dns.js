06d6efe8bb4a402533cc32db963ac04c793ec084
eperret <eperret@salesforce.com>
2013-10-12 03:19:33 -0700

    Moving more files around

--- src/plugins/dns.js
+++ src/plugins/dns.js
@@ -1,106 +1,124 @@
 /*
  * Copyright (c) 2011, Yahoo! Inc.  All rights reserved.
+ * Copyright (c) 2013, Salesforce.com. All rights reserved.
  * Copyrights licensed under the BSD License. See the accompanying LICENSE.txt file for terms.
  */
 
 /**
-\file dns.js
-Plugin to measure DNS latency.
-This code is based on Carlos Bueno's guide to DNS on the YDN blog:
-http://developer.yahoo.net/blog/archives/2009/11/guide_to_dns.html
-*/
+ * \file dns.js
+ * Plugin to measure DNS latency.
+ * This code is based on Carlos Bueno's guide to DNS on the YDN blog:
+ * http://developer.yahoo.net/blog/archives/2009/11/guide_to_dns.html
+ * 
+ * @private
+ */
+function dnsrun() {
 
-(function() {
+    BOOMR = BOOMR || {};
+    BOOMR.plugins = BOOMR.plugins || {};
 
-BOOMR = BOOMR || {};
-BOOMR.plugins = BOOMR.plugins || {};
+    /**
+     * @struct
+     * @private
+     * @const
+     */
+    var impl = {
+        complete: false,
+        base_url: "",
+        t_start: null,
+        t_dns: null,
+        t_http: null,
+        img: null,
 
-var impl = {
-	complete: false,
-	base_url: "",
-	t_start: null,
-	t_dns: null,
-	t_http: null,
-	img: null,
+        gen_url: "",
 
-	gen_url: "",
+        start: function () {
+            if (impl.gen_url) {    // already running
+                return;
+            }
 
-	start: function() {
-		if(impl.gen_url) {	// already running
-			return;
-		}
+            var random = Math.random().toString(36),
+                cache_bust = (new Date().getTime()) + "." + (Math.random());
 
-		var random = Math.random().toString(36),
-		    cache_bust = (new Date().getTime()) + "." + (Math.random());
+            impl.gen_url = impl.base_url.replace(/\*/, random);
 
-		impl.gen_url = impl.base_url.replace(/\*/, random);
+            impl.img = new Image();
+            impl.img.onload = impl.A_loaded;
 
-		impl.img = new Image();
-		impl.img.onload = impl.A_loaded;
+            impl.t_start = new Date().getTime();
+            impl.img.src = impl.gen_url + "image-l.gif?t=" + cache_bust;
+        },
 
-		impl.t_start = new Date().getTime();
-		impl.img.src = impl.gen_url + "image-l.gif?t=" + cache_bust;
-	},
+        A_loaded: function () {
+            var cache_bust;
+            impl.t_dns = new Date().getTime() - impl.t_start;
 
-	A_loaded: function() {
-		var cache_bust;
-		impl.t_dns = new Date().getTime() - impl.t_start;
+            cache_bust = (new Date().getTime()) + "." + (Math.random());
 
-		cache_bust = (new Date().getTime()) + "." + (Math.random());
+            impl.img = new Image();
+            impl.img.onload = impl.B_loaded;
 
-		impl.img = new Image();
-		impl.img.onload = impl.B_loaded;
+            impl.t_start = new Date().getTime();
+            impl.img.src = impl.gen_url + "image-l.gif?t=" + cache_bust;
+        },
 
-		impl.t_start = new Date().getTime();
-		impl.img.src = impl.gen_url + "image-l.gif?t=" + cache_bust;
-	},
+        B_loaded: function () {
+            impl.t_http = new Date().getTime() - impl.t_start;
 
-	B_loaded: function() {
-		impl.t_http = new Date().getTime() - impl.t_start;
+            impl.img = null;
+            impl.done();
+        },
 
-		impl.img = null;
-		impl.done();
-	},
+        done: function () {
+            // DNS time is the time to load the image with uncached DNS
+            // minus the time to load the image with cached DNS
 
-	done: function() {
-		// DNS time is the time to load the image with uncached DNS
-		// minus the time to load the image with cached DNS
+            var dns = impl.t_dns - impl.t_http;
 
-		var dns = impl.t_dns - impl.t_http;
+            BOOMR.addVar("dns.t", dns);
+            impl.complete = true;
+            impl.gen_url = "";
+            BOOMR.sendBeacon();
+        }
+    };
 
-		BOOMR.addVar("dns.t", dns);
-		impl.complete = true;
-		impl.gen_url = "";
-		BOOMR.sendBeacon();
-	}
-};
+    /**
+     * @struct
+     * @const
+     */
+    var dns = BOOMR.plugins.DNS =  /** @implements {IPlugin} */ {
+        /**
+         * @param {Object.<string, ?>|null} config
+         * @return {!Object}
+         */
+        init: function (config) {
+            BOOMR.utils.pluginConfig(impl, config, "DNS", ["base_url"]);
 
-BOOMR.plugins.DNS = {
-	init: function(config) {
-		BOOMR.utils.pluginConfig(impl, config, "DNS", ["base_url"]);
+            if (!impl.base_url) {
+                BOOMR.warn("DNS.base_url is not set.  Cannot run DNS test.", "dns");
+                impl.complete = true;    // set to true so that is_complete doesn't
+                            // block other plugins
+                return dns;
+            }
 
-		if(!impl.base_url) {
-			BOOMR.warn("DNS.base_url is not set.  Cannot run DNS test.", "dns");
-			impl.complete = true;	// set to true so that is_complete doesn't
-						// block other plugins
-			return this;
-		}
+            // do not run test over https
+            if (BOOMR.window.location.protocol === 'https:') {
+                impl.complete = true;
+                return dns;
+            }
 
-		// do not run test over https
-		if(BOOMR.window.location.protocol === 'https:') {
-			impl.complete = true;
-			return this;
-		}
+            BOOMR.subscribe("page_ready", impl.start, null, impl);
 
-		BOOMR.subscribe("page_ready", impl.start, null, impl);
+            return dns;
+        },
 
-		return this;
-	},
+        /**
+         * @return {boolean}
+         */
+        is_complete: function () {
+            return impl.complete;
+        }
+    };
 
-	is_complete: function() {
-		return impl.complete;
-	}
-};
-
-}());
-
+}
+dnsrun();
--- src/plugins/ipv6.js
+++ src/plugins/ipv6.js
@@ -1,185 +1,202 @@
 /*
  * Copyright (c) 2011, Yahoo! Inc.  All rights reserved.
+ * Copyright (c) 2013, Salesforce.com. All rights reserved.
  * Copyrights licensed under the BSD License. See the accompanying LICENSE.txt file for terms.
  */
 
 /**
-\file ipv6.js
-Plugin to measure various ipv6 related metrics.
-This plugin tries to do a few things:
-- Check if the client can connect to an ipv6 address
-- Check if the client can resolve DNS that points to an ipv6 address
-- Check latency of connecting to an ipv6 address
-- Check avg latency of doing dns lookup to an ipv6 address (not worstcase)
-
-You'll need a server that has an ipv6 address, and a DNS name to point to it.
-Additionally, this server needs to be configured to serve content requested
-from the IPv6 address and should not require a virtual host name.  This means
-that you probably cannot use shared hosting that puts multiple hosts on the
-same IP address.
-
-All beacon parameters are prefixed with ipv6_
-
-Beacon parameters:
-- ipv6_latency: Latency in milliseconds of getting data from an ipv6 host when
-		connecting to the IP.  Set to NA if the client cannot connect
-		to the ipv6 host.
-- ipv6_lookup:  Latency of getting data from a hostname that resolves to an
-		ipv6 address.  Set to NA if the client cannot resolve or connect
-		to the ipv6 host.
+* \file ipv6.js
+* Plugin to measure various ipv6 related metrics.
+* This plugin tries to do a few things:
+* - Check if the client can connect to an ipv6 address
+* - Check if the client can resolve DNS that points to an ipv6 address
+* - Check latency of connecting to an ipv6 address
+* - Check avg latency of doing dns lookup to an ipv6 address (not worstcase)
+* 
+* You'll need a server that has an ipv6 address, and a DNS name to point to it.
+* Additionally, this server needs to be configured to serve content requested
+* from the IPv6 address and should not require a virtual host name.  This means
+* that you probably cannot use shared hosting that puts multiple hosts on the
+* same IP address.
+* 
+* All beacon parameters are prefixed with ipv6_
+* 
+* Beacon parameters:
+* - ipv6_latency: Latency in milliseconds of getting data from an ipv6 host when
+*       connecting to the IP.  Set to NA if the client cannot connect
+*       to the ipv6 host.
+* - ipv6_lookup:  Latency of getting data from a hostname that resolves to an
+*       ipv6 address.  Set to NA if the client cannot resolve or connect
+*       to the ipv6 host.
+* 
+* @private
 */
+function ipv6run() {
 
-(function() {
+    BOOMR = BOOMR || {};
+    BOOMR.plugins = BOOMR.plugins || {};
 
-BOOMR = BOOMR || {};
-BOOMR.plugins = BOOMR.plugins || {};
+    /**
+     * Algorithm:
+     * 
+     * 1. Try to load a sizeless image from an IPv6 host
+     *    - onerror, flag no IPv6 connect support and end
+     *    - onload, measure load time
+     * 2. Try to load a sizeless image from a hostname that resolves to an IPv6 address
+     *    - onerror, flag no IPv6 DNS resolver and end
+     *    - onload, measure load time
+     * 
+     * @private
+     * @const
+     */
+    var impl = {
+        complete: false,
+        ipv6_url: "",
+        host_url: "",
+        timeout: 1200,
 
-/*
-Algorithm:
+        timers: {
+            ipv6: { start: null, end: null },
+            host: { start: null, end: null }
+        },
 
-1. Try to load a sizeless image from an IPv6 host
-   - onerror, flag no IPv6 connect support and end
-   - onload, measure load time
-2. Try to load a sizeless image from a hostname that resolves to an IPv6 address
-   - onerror, flag no IPv6 DNS resolver and end
-   - onload, measure load time
-*/
-var impl = {
-	complete: false,
-	ipv6_url: "",
-	host_url: "",
-	timeout: 1200,
+        start: function () {
+            impl.load_img('ipv6', 'host');
+        },
 
-	timers: {
-		ipv6: { start: null, end: null },
-		host: { start: null, end: null }
-	},
+        /**
+         * @param {...!string} arg0
+         */
+        load_img: function (arg0) {
+            var img,
+                rnd = "?t=" + (new Date().getTime()) + Math.random(),
+                timer = 0,
+                error = null,
+                that = impl,
+                which = Array.prototype.shift.call(arguments),
+                a = arguments;
 
-	start: function() {
-		this.load_img('ipv6', 'host');
-	},
+            // Terminate if we've reached end of test list
+            if (!which || !impl.timers.hasOwnProperty(which)) {
+                impl.done();
+                return false;
+            }
 
-	load_img: function() {
-		var img,
-			rnd = "?t=" + (new Date().getTime()) + Math.random(),
-			timer=0, error = null,
-			that = this,
-			which = Array.prototype.shift.call(arguments),
-			a = arguments;
+            // Skip if URL wasn't set for this test
+            if (!impl[which + '_url']) {
+                return impl.load_img.apply(impl, a);
+            }
 
-		// Terminate if we've reached end of test list
-		if(!which || !this.timers.hasOwnProperty(which)) {
-			this.done();
-			return false;
-		}
+            img = new Image();
 
-		// Skip if URL wasn't set for this test
-		if(!this[which + '_url']) {
-			return this.load_img.apply(this, a);
-		}
+            img.onload = function () {
+                that.timers[which].end = new Date().getTime();
+                clearTimeout(timer);
+                img.onload = img.onerror = null;
+                img = null;
 
-		img = new Image();
+                that.load_img.apply(that, a);
+                that = a = null;
+            };
 
-		img.onload = function() {
-			that.timers[which].end = new Date().getTime();
-			clearTimeout(timer);
-			img.onload = img.onerror = null;
-			img = null;
+            error = function () {
+                that.timers[which].supported = false;
+                clearTimeout(timer);
+                img.onload = img.onerror = null;
+                img = null;
 
-			that.load_img.apply(that, a);
-			that = a = null;
-		};
+                that.done();
+                that = a = null;
+            };
 
-		error = function() {
-			that.timers[which].supported = false;
-			clearTimeout(timer);
-			img.onload = img.onerror = null;
-			img = null;
+            img.onerror = error;
+            timer = setTimeout(error, impl.timeout);
+            impl.timers[which].start = new Date().getTime();
+            img.src = impl[which + '_url'] + rnd;
 
-			that.done();
-			that = a = null;
-		};
+            return true;
+        },
 
-		img.onerror = error;
-		timer = setTimeout(error, this.timeout);
-		this.timers[which].start = new Date().getTime();
-		img.src = this[which + '_url'] + rnd;
+        done: function () {
+            if (impl.complete) {
+                return;
+            }
 
-		return true;
-	},
+            BOOMR.removeVar('ipv6_latency', 'ipv6_lookup');
+            if (impl.timers.ipv6.end !== null) {
+                BOOMR.addVar('ipv6_latency', impl.timers.ipv6.end - impl.timers.ipv6.start);
+            } else {
+                BOOMR.addVar('ipv6_latency', 'NA');
+            }
 
-	done: function() {
-		if(this.complete) {
-			return;
-		}
+            if (impl.timers.host.end !== null) {
+                BOOMR.addVar('ipv6_lookup', impl.timers.host.end - impl.timers.host.start);
+            } else {
+                BOOMR.addVar('ipv6_lookup', 'NA');
+            }
 
-		BOOMR.removeVar('ipv6_latency', 'ipv6_lookup');
-		if(this.timers.ipv6.end !== null) {
-			BOOMR.addVar('ipv6_latency', this.timers.ipv6.end - this.timers.ipv6.start);
-		}
-		else {
-			BOOMR.addVar('ipv6_latency', 'NA');
-		}
+            impl.complete = true;
+            BOOMR.sendBeacon();
+        },
 
-		if(this.timers.host.end !== null) {
-			BOOMR.addVar('ipv6_lookup', this.timers.host.end - this.timers.host.start);
-		}
-		else {
-			BOOMR.addVar('ipv6_lookup', 'NA');
-		}
+        skip: function () {
+            // it's possible that we didn't start, so sendBeacon never
+            // gets called.  Let's set our complete state and call sendBeacon.
+            // This happens if onunload fires before onload
 
-		this.complete = true;
-		BOOMR.sendBeacon();
-	},
+            if (!impl.complete) {
+                impl.complete = true;
+                BOOMR.sendBeacon();
+            }
 
-	skip: function() {
-		// it's possible that we didn't start, so sendBeacon never
-		// gets called.  Let's set our complete state and call sendBeacon.
-		// This happens if onunload fires before onload
+            return impl;
+        }
+    };
 
-		if(!this.complete) {
-			this.complete = true;
-			BOOMR.sendBeacon();
-		}
+    /**
+     * @struct
+     * @const
+     */
+    var ipv6 = BOOMR.plugins.IPv6 =  /** @implements {IPlugin} */ {
+        /**
+         * @param {Object.<string, ?>|null} config
+         * @return {?Object}
+         */
+        init: function (config) {
+            BOOMR.utils.pluginConfig(impl, config, "IPv6", ["ipv6_url", "host_url", "timeout"]);
 
-		return this;
-	}
-};
+            if (!impl.ipv6_url) {
+                BOOMR.warn("IPv6.ipv6_url is not set.  Cannot run IPv6 test.", "ipv6");
+                impl.complete = true;    // set to true so that is_complete doesn't
+                            // block other plugins
+                return ipv6;
+            }
 
-BOOMR.plugins.IPv6 = {
-	init: function(config) {
-		BOOMR.utils.pluginConfig(impl, config, "IPv6", ["ipv6_url", "host_url", "timeout"]);
+            if (!impl.host_url) {
+                BOOMR.warn("IPv6.host_url is not set.  Will skip hostname test.", "ipv6");
+            }
 
-		if(!impl.ipv6_url) {
-			BOOMR.warn("IPv6.ipv6_url is not set.  Cannot run IPv6 test.", "ipv6");
-			impl.complete = true;	// set to true so that is_complete doesn't
-						// block other plugins
-			return this;
-		}
+            // make sure that test images use the same protocol as the host page
+            if (BOOMR.window.location.protocol === 'https:') {
+                impl.complete = true;
+                return ipv6;
+            }
 
-		if(!impl.host_url) {
-			BOOMR.warn("IPv6.host_url is not set.  Will skip hostname test.", "ipv6");
-		}
+            impl.ipv6_url = impl.ipv6_url.replace(/^https:/, 'http:');
+            impl.host_url = impl.host_url.replace(/^https:/, 'http:');
 
-		// make sure that test images use the same protocol as the host page
-		if(BOOMR.window.location.protocol === 'https:') {
-			impl.complete = true;
-			return this;
-		}
+            BOOMR.subscribe("page_ready", impl.start, null, impl);
+            BOOMR.subscribe("page_unload", impl.skip, null, impl);
 
-		impl.ipv6_url = impl.ipv6_url.replace(/^https:/, 'http:');
-		impl.host_url = impl.host_url.replace(/^https:/, 'http:');
+            return ipv6;
+        },
 
-		BOOMR.subscribe("page_ready", impl.start, null, impl);
-		BOOMR.subscribe("page_unload", impl.skip, null, impl);
-
-		return this;
-	},
-
-	is_complete: function() {
-		return impl.complete;
-	}
-};
-
-}());
-
+        /**
+         * @return {boolean}
+         */
+        is_complete: function () {
+            return impl.complete;
+        }
+    };
+}
+ipv6run();
